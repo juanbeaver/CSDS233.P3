549ff8dc7880c18ee140c4a01df226420f511ec5
Juan Beaver <jcarlosbeaver@live.com>
2022-03-03 20:09:36 -0500

    See log for Feb. 25, 5:00 AM

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -2,7 +2,60 @@ import java.util.List;
 
 public class BinarySearchTree<T extends Comparable<T>, V extends T> {
 
-    private BinaryNode<T, V> root;
+    private class BinaryNode{
+
+        private final T key;
+
+        private final V value;
+
+        private BinaryNode left = null;
+
+        private BinaryNode right = null;
+
+        public BinaryNode(){
+            key = null;
+            value = null;
+        }
+
+    }
+
+    public BinaryNode root = null;
+
+    public static void main(String[] args){
+        BinarySearchTree<Integer, Integer> test = new BinarySearchTree<>();
+        test.insert(2, 2);
+        test.insert(1, 1);
+        test.insert(4, 4);
+        test.insert(5, 5);
+        test.insert(9, 9);
+        test.insert(3, 3);
+        test.insert(6, 6);
+        test.insert(7, 7);
+        test.insert(10, 10);
+        test.insert(12, 12);
+        test.insert(11, 11);
+
+        test.preorderPrint(test.root);;
+
+    }
+
+    public String preorderPrint(BinaryNode root){
+        StringBuilder s2 = new StringBuilder();
+
+        if(root != null){
+            BinaryNode parent = this.root;
+            s2.append(root.key + " ");
+            System.out.print(root.key + " ");
+
+            if(root.left != null){
+                preorderPrint(root.left);
+            }
+            if(root.right != null){
+                preorderPrint(root.right);
+            }
+        }
+        return s2.toString();
+    }
 
     public BinarySearchTree(){
 
--- BinarySearchTreeTester.java
+++ BinarySearchTreeTester.java
@@ -1,26 +1,90 @@
 import org.junit.Test;
 
+import java.lang.management.BufferPoolMXBean;
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.*;
 
 public class BinarySearchTreeTester {
 
     @Test
     public void testInsert(){
+        BinarySearchTree<Integer, Integer> testTree = new BinarySearchTree<>();
 
+        //Test if tree is empty
+        testTree.insert(4, 4);
+
+        assertEquals("4 ", testTree.preorderPrint(testTree.root));
+
+        //Test inserting when parent node has no children
+        testTree.insert(5, 5);
+
+        assertEquals("4 5 ", testTree.preorderPrint(testTree.root));
+
+        //Test inserting when parent node has one child
+        testTree.insert(3,3);
+
+        assertEquals("4 3 5 ", testTree.preorderPrint(testTree.root));
     }
 
     @Test
     public void testSearch(){
+        BinarySearchTree<Integer, Integer> testTree = new BinarySearchTree<>();
 
+        //Test if the root is null
+        assertThrows(NoSuchElementException.class, () ->  testTree.search(4));
+
+        //Test when the node to be found is the root node
+        testTree.insert(5,5);
+        assertEquals((Integer) 5, testTree.search(5));
+
+        //Test proper tree traversal for all other nodes in the tree
+        testTree.insert(12, 12);    testTree.insert(6,6);   testTree.insert(9,9);
+        testTree.insert(20,20);
+
+        assertEquals((Integer) 20, testTree.search(20));
     }
 
     @Test
     public void testDelete(){
+        BinarySearchTree<Integer, Integer> testTree = new BinarySearchTree<>();
+
+        //Test when node is the only node in the tree
+        testTree.insert(2,2);
+
+        testTree.delete(2);
+
+        assertEquals("", testTree.preorderPrint(testTree.root));
+
+        //Test when node is not found
+        testTree.insert(5,5);   testTree.insert(2,2);   testTree.insert(8,8);
+
+        testTree.delete(16);
+
+        assertEquals("5 2 8 ", testTree.preorderPrint(testTree.root));
+
+        //Test when node has no children
+        testTree.delete(2);
+
+        assertEquals("5 8 ", testTree.preorderPrint(testTree.root));
+
+        //Test when node has only one child
+        testTree.insert(6,6);
+
+        assertEquals("5 6 ", testTree.preorderPrint(testTree.root));
+
+        //Test when node has two children
+        testTree.insert(2,2);   testTree.insert(1,1);   testTree.insert(4,4);
+
+        testTree.delete(2);
+
+        assertEquals("5 4 1 6 ", testTree.preorderPrint(testTree.root));
 
     }
 
     @Test
     public void testInorderRec(){
+        
 
     }
 
